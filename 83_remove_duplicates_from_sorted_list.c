2f6901e934fac1b866350b6c6fbaa9b6e035bd58
Akshaya K <akshaya4650@gmail.com>
2026-02-19 10:39:13 +0530

    Add 83_remove_duplicates_from_sorted_list.c - LeetCode Problem 83

---
+++ 83_remove_duplicates_from_sorted_list.c
@@ -0,0 +1,12 @@
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    struct ListNode *cur=head;
+    while(cur!=NULL && (cur->next!=NULL)){
+        if(cur->val==cur->next->val){
+            cur->next=cur->next->next;
+        }
+        else{
+            cur=cur->next;
+        }
+    }
+    return head;
+}
